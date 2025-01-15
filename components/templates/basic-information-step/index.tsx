11e255ee0bd507081af363ae3c7cc48567406108
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2025-01-15 04:03:45 -0300

    feat(atoms): add toggle-button component

--- components/templates/basic-information-step/index.tsx
+++ components/templates/basic-information-step/index.tsx
@@ -1,15 +1,21 @@
 import { steps } from "@/app/page";
 import { Container } from "@/components/atoms/container";
+import { ToggleButton } from "@/components/atoms/toggle-button";
 import { BannerInput } from "@/components/molecules/banner-input";
 import { CheckboxInput } from "@/components/molecules/checkbox-input";
 import { SelectInput } from "@/components/molecules/select-input";
 import { StepContent } from "@/components/molecules/step-content";
 import { TextInput } from "@/components/molecules/text-input";
 
+const toggleOptions = ["Enable Event", "Disable Event"];
 export const BasicInformationStep = () => {
   return (
     <StepContent step={steps[0]}>
       <Container className="grid grid-cols-2 gap-5">
+        <div className="col-span-2 flex items-center justify-center">
+          <ToggleButton options={toggleOptions} />
+        </div>
+
         <SelectInput
           items={[]}
           className="col-span-1"
