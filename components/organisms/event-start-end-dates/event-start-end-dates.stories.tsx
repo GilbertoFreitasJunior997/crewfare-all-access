fb0fea1453de3fff17b5c652e946dd7bddfd178c
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2025-01-17 01:19:06 -0300

    docs(event-start-end-dates): add story

---
+++ components/organisms/event-start-end-dates/event-start-end-dates.stories.tsx
@@ -0,0 +1,71 @@
+import { FormProvider } from "@/components/atoms/form-provider";
+import { StepperProvider } from "@/components/atoms/stepper-provider";
+import { useForm } from "@/hooks/use-form";
+import { steps } from "@/lib/steps";
+import type { Meta, StoryObj } from "@storybook/react";
+import { addDays } from "date-fns";
+import { EventStartEndDates } from ".";
+
+const meta: Meta<typeof EventStartEndDates> = {
+  title: "Design System/Organisms/EventStartEndDates",
+  component: EventStartEndDates,
+  decorators: [
+    (Story) => {
+      const form = useForm();
+
+      return (
+        <FormProvider form={form}>
+          <StepperProvider steps={steps}>
+            <Story />
+          </StepperProvider>
+        </FormProvider>
+      );
+    },
+  ],
+  args: {
+    hasBookableRange: true,
+    disabledDates: undefined,
+  },
+  argTypes: {
+    hasBookableRange: {
+      control: "boolean",
+      description:
+        "Controls whether users can select date ranges. If `false`, all inputs are disabled.",
+    },
+    disabledDates: {
+      control: "object",
+      description:
+        "An optional object defining date intervals that are disabled for selection.",
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "The `EventStartEndDates` component manages a list of date ranges for events. It supports adding and removing rows dynamically and handles validation through the form provider.",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof EventStartEndDates>;
+
+export const Default: Story = {};
+
+export const LimitedPeriod: Story = {
+  args: {
+    hasBookableRange: true,
+    disabledDates: {
+      before: new Date(),
+      after: addDays(new Date(), 30),
+    },
+  },
+  parameters: {
+    docs: {
+      storyDescription:
+        "Demonstrates the `EventStartEndDates` component with a limited period, where date ranges before today and a month after are disabled.",
+    },
+  },
+};
