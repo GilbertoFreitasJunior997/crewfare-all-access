af1597b1b2fef55bf9b578fa9d5f9f6834aaf03f
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2025-01-16 22:32:43 -0300

    docs(tooltip): add story

---
+++ components/atoms/tooltip/tooltip.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Tooltip } from ".";
+import { Button } from "../button";
+
+const meta: Meta = {
+  title: "Design System/Atoms/Tooltip",
+  component: () => (
+    <Tooltip.Provider>
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild>
+          <Button variant="secondary">Hover over me</Button>
+        </Tooltip.Trigger>
+        <Tooltip.Content className="w-80">
+          This is a tooltip with some helpful information. Hover over the button
+          to see it.
+        </Tooltip.Content>
+      </Tooltip.Root>
+    </Tooltip.Provider>
+  ),
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "The `Tooltip` component is used to display additional information when a user hovers over an element. It consists of a `Root` to wrap the `Trigger` and `Content`, with the `Trigger` activating the `Content` to show the tooltip.",
+      },
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const TooltipDemo: Story = {};
