2b471d4f4b8f80127f7d779598052ad36f7e9288
GilbertoFreitasJunior997 <gilbertofreitas997@gmail.com>
2025-01-17 09:03:59 -0300

    test(popover): add tests

---
+++ components/atoms/popover/popover.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { Popover } from ".";
+import { Button } from "../button";
+
+// actions like clicking arent tested because its lib scoped
+describe("Popover", () => {
+  it("renders correctly", () => {
+    render(
+      <Popover.Root>
+        <Popover.Trigger asChild>
+          <Button variant="secondary">Open Popover</Button>
+        </Popover.Trigger>
+        <Popover.Content className="w-80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ea
+          necessitatibus vel vitae, expedita, rem delectus architecto laboriosam
+          suscipit consectetur doloribus sapiente quos cum optio minus! Suscipit
+          odio natus quod!
+        </Popover.Content>
+      </Popover.Root>,
+    );
+  });
+
+  const trigger = screen.getByRole("button");
+  expect(trigger).toBeInTheDocument();
+});
